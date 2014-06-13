3f067f242404f5231c0e8503a238b19328ac9507
Selim Dincer <wowselim@live.de>
2014-06-13 16:18:50 +0200

    initial commit

---
+++ NTWeaponCheat/src/boot/Boot.java
@@ -0,0 +1,75 @@
+package boot;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Boot {
+	public static void main(String[] args) {
+		boolean error = false;
+		String appdata = System.getProperty("user.home");
+		appdata += "\\AppData\\Local\\nuclearthrone";
+
+		List<String> fileNames = new ArrayList<>();
+		try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths
+				.get(appdata))) {
+			for (Path path : dirStream) {
+				String directory = path.toString();
+				if (directory.contains("nuclearthrone")
+						&& directory.endsWith(".sav"))
+					fileNames.add(path.toString());
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading files.");
+			error = true;
+		}
+
+		Collections.sort(fileNames);
+		final Path versionFile = Paths.get(fileNames.get(fileNames.size() - 1));
+		System.out.println("Path of file: " + versionFile.toString());
+		List<String> lines = null;
+
+		try {
+			lines = Files.readAllLines(versionFile);
+		} catch (IOException e) {
+			System.out.println("Error reading current version file.");
+			error = true;
+		}
+
+		boolean toggle = true;
+		boolean found = false;
+		for (int i = 0; i < lines.size(); i++) {
+			if (found && toggle) {
+				lines.set(i, "17");
+			}
+
+			if (found)
+				toggle = !toggle;
+
+			if (lines.get(i).equals("DATA"))
+				found = true;
+		}
+
+		try {
+			if (found && versionFile.toString().endsWith(".sav"))
+				Files.write(versionFile, lines);
+			else {
+				System.out.println("Could not identify version file.");
+				error = true;
+			}
+		} catch (IOException e) {
+			System.out.println("Error writing version file.");
+			error = true;
+		}
+
+		if (!error) {
+			System.out.println("Version file has been successfully modified.");
+		}
+
+	}
+}
