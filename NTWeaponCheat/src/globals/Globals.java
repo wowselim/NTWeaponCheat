a70ade8fc0ac367d411a19d4326f3f474592991e
Selim Dincer <wowselim@live.de>
2014-06-14 14:04:33 +0200

    fixed compatibility

--- NTWeaponCheat/src/globals/Globals.java
+++ NTWeaponCheat/src/globals/Globals.java
@@ -10,10 +10,10 @@ public class Globals {
 	public static String getDirectory() {
 		String os = System.getProperty("os.name");
 		String sep = getSeparator();
-		if (os.equalsIgnoreCase("linux"))
+		if (os.toLowerCase().contains("linux"))
 			return System.getProperty("user.home") + sep + ".config" + sep
 					+ "nuclearthrone";
-		else if (os.equalsIgnoreCase("windows"))
+		else if (os.toLowerCase().contains("windows"))
 			return System.getProperty("user.home") + sep + "AppData" + sep
 					+ "Local" + sep + "nuclearthrone";
 		return "";
